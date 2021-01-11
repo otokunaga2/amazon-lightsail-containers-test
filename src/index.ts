562bd9a7496d1e576efb58b30bd3b03acda4ba3a
Seiki Tokunaga <otokuna46@gmail.com>
2021-01-11 15:49:41 +0900

    Fix invalid url response

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,23 @@ import { createServer } from "http";
 
 const server = createServer((req, res) => {
   res.setHeader("Content-Type", "application/json");
-  res.write(JSON.stringify({
-    message: "hello world!",
-    success: req.url === "/"
-  }));
-  res.write(JSON.stringify({
-    message: "healthy:)",
-    success: req.url === "/health"
-  }));
-  res.end();
+  switch(req.url){
+    case "/":
+      if(req.method === "GET"){
+      let response = {message: "hello world!", success: true}
+      res.end(JSON.stringify(response));
+    }
+    break;
+    case "/health":
+      if(req.method === "GET"){
+      let response = {message: "healthy", success: true}
+      res.end(JSON.stringify(response));
+    }
+    break;
+    default:
+      let response = {message: 'Your request is not supported:(', success: false};
+    res.end(JSON.stringify(response));
+  }
 });
 
 server.listen(process.env.PORT || 3000);
